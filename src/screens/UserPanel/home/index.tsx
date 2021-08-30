26a451b03858ee25e1dc8ea69f53fa15a0467f64
Karolina <karolina@Arturs-Mac-mini.local>
2021-08-30 20:54:06 +0200

    button shows list of notification removed

--- src/screens/UserPanel/home/index.tsx
+++ src/screens/UserPanel/home/index.tsx
@@ -1,7 +1,3 @@
-import { useState } from "react";
-
-import { Button } from "react-bootstrap";
-
 import { ListOfNotifications } from "../ListOfNotification";
 import { AddNotificationView } from "../AddNotification";
 import { MenuList } from "../../../modals/menuList";
@@ -9,27 +5,13 @@ import { MenuList } from "../../../modals/menuList";
 export const Home = (props: any) => {
   const { userName } = props;
 
-  const [listVisible, setListVisible] = useState(false);
-
   return (
     <>
       <MenuList userName={userName} />
 
       <AddNotificationView />
 
-      <Button
-        variant="outline-info"
-        size="lg"
-        onClick={() => {
-          if (listVisible == false) {
-            return setListVisible(true);
-          }
-          return setListVisible(false);
-        }}
-      >
-        Lista Przypomnień
-      </Button>
-      {listVisible ? <ListOfNotifications /> : null}
+      <ListOfNotifications />
     </>
   );
 };
