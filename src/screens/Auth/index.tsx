217c5b69c58218440a6e838f38b752f72a8d4f08
Karolina <karolina@Arturs-MacBook-Pro.local>
2021-09-21 17:50:57 +0200

    xxx

--- src/screens/Auth/index.tsx
+++ src/screens/Auth/index.tsx
@@ -1,5 +1,3 @@
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-
 import { Col, Row, Container, Button } from "react-bootstrap";
 
 import { Register } from "./register";
